7038c1b9c3a2162b5e45eb26dc29d1a584d0fc4a
Caroline Zana <caroline.zana@sjsu.edu>
2023-10-04 17:40:38 -0700

    Question 3

--- sorted.py
+++ sorted.py
@@ -2,7 +2,7 @@ def sort_dictionary(d):
 
     sorted_by_age = sorted(d.items(), key=lambda x: x[1][1])
 
-    result = [(name, phone) for name, (phone, age) in sorted_by_age]
+    result = [(name, phone) for name, (phone, age) in sorted_by_name]
 
     return result
 
