9e3eb3af8e55e935025e70c49570af2b1e0673d9
Caroline Zana <caroline.zana@sjsu.edu>
2023-10-04 17:23:23 -0700

    Question 3

--- sorted.py
+++ sorted.py
@@ -1,8 +1,6 @@
 def sort_dictionary(input_dict):
-    
-    sorted_dict = dict(sorted(input_dict.items(), key=lambda x: x[0]))
+    sorted_dict = dict(sorted(input_dict.items(), key=lambda x: x[0].lower()))
 
-   
     sorted_list = [(name, info[0]) for name, info in sorted_dict.items()]
 
     return sorted_list
